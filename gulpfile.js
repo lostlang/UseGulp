bf147db012fb67dea94b6e666652b083f03d5b14
lostlang <53492781+lostlang@users.noreply.github.com>
2021-07-15 13:06:28 +0300

    Add and use prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,158 +1,99 @@
-const {series, parallel, watch, src, dest} = require('gulp');
-const browserSync = require('browser-sync').create();
-const rename = require('gulp-rename');
-const pug = require('gulp-pug');
-const sass = require('gulp-sass')(require('sass'));
+const { series, parallel, watch, src, dest } = require("gulp");
+const browserSync = require("browser-sync").create();
+const rename = require("gulp-rename");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass")(require("sass"));
 
 const paths = {
   src: {
-    pug: 'src/**/*.pug',
-    sass: 'src/**/*.sass',
+    pug: "src/**/*.pug",
+    sass: "src/**/*.sass",
   },
   main: {
-    pug: 'src/**/index.pug',
-    sass: 'src/**/main.sass',
+    pug: "src/**/index.pug",
+    sass: "src/**/main.sass",
   },
-  build: 'build',
-  dev: 'dev',
+  build: "build",
+  dev: "dev",
 };
 
-/**
- * Get last dir name
- * @param {string} dirName directory file
- * @return {string} last directory
- */
+// Get last dir name
 function lastDirName(dirName) {
-  array = dirName.split('\\');
+  array = dirName.split("\\");
   return array[array.length - 1];
-};
-
+}
 
 // Pug
-/**
- * Build pug files to html (prod)
- * @return {any} pipeline
- */
 function pugBuild() {
   return src(paths.main.pug)
-      .pipe(
-          pug({
-          }),
-      )
-      .pipe(
-          rename(
-              (path) => {
-                return {
-                  dirname: '',
-                  basename: lastDirName(path.dirname),
-                  extname: '.html',
-                };
-              },
-          ),
-      )
-      .pipe(
-          dest(paths.build),
-      );
-};
+    .pipe(pug({}))
+    .pipe(
+      rename((path) => {
+        return {
+          dirname: "",
+          basename: lastDirName(path.dirname),
+          extname: ".html",
+        };
+      })
+    )
+    .pipe(dest(paths.build));
+}
 
-/**
- * Build pug files to html (dev)
- * @return {any} pipeline
- */
 function pugDev() {
   return src(paths.main.pug)
-      .pipe(
-          pug({
-            pretty: true,
-          },
-          ),
-      )
-      .pipe(
-          rename(
-              (path) => {
-                return {
-                  dirname: '',
-                  basename: lastDirName(path.dirname),
-                  extname: '.html',
-                };
-              },
-          ),
-      )
-      .pipe(
-          dest(paths.dev),
-      )
-      .pipe(
-          browserSync.stream(),
-      );
-};
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(
+      rename((path) => {
+        return {
+          dirname: "",
+          basename: lastDirName(path.dirname),
+          extname: ".html",
+        };
+      })
+    )
+    .pipe(dest(paths.dev))
+    .pipe(browserSync.stream());
+}
 
 // Sass
-/**
- * Build sass files to html (dev)
- * @return {any} pipeline
- */
 function sassDev() {
   return src(paths.main.sass)
-      .pipe(
-          sass(),
-      )
-      .pipe(
-          rename(
-              {
-                dirname: '',
-                basename: 'main',
-                extname: '.css',
-              },
-          ),
-      )
-      .pipe(
-          dest(paths.dev),
-      )
-      .pipe(
-          browserSync.stream(),
-      );
-};
+    .pipe(sass())
+    .pipe(
+      rename({
+        dirname: "",
+        basename: "main",
+        extname: ".css",
+      })
+    )
+    .pipe(dest(paths.dev))
+    .pipe(browserSync.stream());
+}
 
 // Watch
-/**
- * Watcher for file
- */
 function watcher() {
   watch(paths.src.pug, pugDev);
   watch(paths.src.sass, sassDev);
-};
+}
 
 // Server
-/**
- * Live reload browser
- */
 function server() {
   browserSync.init({
     server: {
       baseDir: paths.dev,
     },
     notify: false,
-  },
-  );
-};
+  });
+}
 
-const dev = parallel(
-    pugDev,
-    sassDev,
-);
+const dev = parallel(pugDev, sassDev);
 
-const build = parallel(
-    pugBuild,
-);
+const build = parallel(pugBuild);
 
-exports.dev = series(
-    dev,
-    parallel(
-        watcher,
-        server,
-    ),
-);
+exports.dev = series(dev, parallel(watcher, server));
 
-exports.build = series(
-    build,
-);
+exports.build = series(build);

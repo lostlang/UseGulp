e4d57b21b33f73bcc0756ae63718304b2b17118e
lostlang <53492781+lostlang@users.noreply.github.com>
2021-07-16 21:00:51 +0300

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,92 @@
-const { series, parallel, watch, src, dest } = require("gulp");
-const browserSync = require("browser-sync").create();
-const rename = require("gulp-rename");
-const pug = require("gulp-pug");
-const sass = require("gulp-sass")(require("sass"));
+const {series, parallel, watch, src, dest} = require('gulp');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
 
 const paths = {
   src: {
-    pug: "src/**/*.pug",
-    sass: "src/**/*.sass",
+    pug: 'src/pug/**/*.pug',
+    sass: 'src/sass/**/*.sass',
+    img: 'src/**/*.{png,jpg}',
   },
   main: {
-    pug: "src/**/index.pug",
-    sass: "src/**/main.sass",
+    pug: 'src/**/index.pug',
+    sass: 'src/**/main.sass',
   },
-  build: "build",
-  dev: "dev",
+  build: 'build',
+  dev: 'dev',
 };
 
 // Get last dir name
 function lastDirName(dirName) {
-  array = dirName.split("\\");
+  array = dirName.split('\\');
   return array[array.length - 1];
 }
 
 // Pug
 function pugBuild() {
   return src(paths.main.pug)
-    .pipe(pug({}))
-    .pipe(
-      rename((path) => {
-        return {
-          dirname: "",
-          basename: lastDirName(path.dirname),
-          extname: ".html",
-        };
-      })
-    )
-    .pipe(dest(paths.build));
+      .pipe(pug({}))
+      .pipe(
+          rename((path) => {
+            return {
+              dirname: '',
+              basename: lastDirName(path.dirname),
+              extname: '.html',
+            };
+          }),
+      )
+      .pipe(dest(paths.build));
 }
 
 function pugDev() {
   return src(paths.main.pug)
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
-    .pipe(
-      rename((path) => {
-        return {
-          dirname: "",
-          basename: lastDirName(path.dirname),
-          extname: ".html",
-        };
-      })
-    )
-    .pipe(dest(paths.dev))
-    .pipe(browserSync.stream());
+      .pipe(
+          pug({
+            pretty: true,
+          }),
+      )
+      .pipe(
+          rename((path) => {
+            return {
+              dirname: '',
+              basename: lastDirName(path.dirname),
+              extname: '.html',
+            };
+          }),
+      )
+      .pipe(dest(paths.dev))
+      .pipe(browserSync.stream());
 }
 
 // Sass
 function sassDev() {
   return src(paths.main.sass)
-    .pipe(sass())
-    .pipe(
-      rename({
-        dirname: "",
-        basename: "main",
-        extname: ".css",
-      })
-    )
-    .pipe(dest(paths.dev))
-    .pipe(browserSync.stream());
+      .pipe(sass())
+      .pipe(
+          rename({
+            dirname: '',
+            basename: 'main',
+            extname: '.css',
+          }),
+      )
+      .pipe(dest(paths.dev))
+      .pipe(browserSync.stream());
+}
+
+// Image
+function imageDev() {
+  return src(paths.src.img)
+      .pipe(dest(paths.dev))
+      .pipe(browserSync.stream());
 }
 
 // Watch
 function watcher() {
   watch(paths.src.pug, pugDev);
   watch(paths.src.sass, sassDev);
+  watch(paths.src.img, imageDev);
 }
 
 // Server
@@ -90,7 +99,7 @@ function server() {
   });
 }
 
-const dev = parallel(pugDev, sassDev);
+const dev = parallel(pugDev, sassDev, imageDev);
 
 const build = parallel(pugBuild);
 
